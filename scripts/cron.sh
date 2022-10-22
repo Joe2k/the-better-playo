191b48cfec6d1fdd6fbff63c62e11daee7dd211a
Jonathan Samuel <jonathansamuel2k@gmail.com>
2022-10-22 17:06:02 +0530

    [UPD] add git force push

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -11,4 +11,4 @@ git config user.email "jonathansamuel2k@gmail.com"
 git config user.name "Jonathan Samuel"
 git add data/
 git commit -m "Auto update data for the website" || echo "Nothing to commit"
-git push origin main
+git push --force origin main
