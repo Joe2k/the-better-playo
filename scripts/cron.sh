88846fdb7bb9d141f0c1f9b5372d0146e484ed0b
Jonathan Samuel <jonathansamuel2k@gmail.com>
2022-10-22 09:38:36 +0530

    [UPD] cron

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -9,6 +9,6 @@ node scripts/cron.js
 
 git config user.email "jonathansamuel2k@gmail.com"
 git config user.name "Jonathan Samuel"
-git add data/
+git add .
 git commit -m "Auto update data for the website" || echo "Nothing to commit"
 git push origin main
